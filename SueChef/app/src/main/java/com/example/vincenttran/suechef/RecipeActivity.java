25dbcca48e2bc5a771be55a7361d4a7bfe08218e
chentetran <vincenttran23@gmail.com>
2016-10-13 14:35:34 -0400

    expandablelist

--- SueChef/app/src/main/java/com/example/vincenttran/suechef/RecipeActivity.java
+++ SueChef/app/src/main/java/com/example/vincenttran/suechef/RecipeActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,7 +19,16 @@ import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class RecipeActivity extends AppCompatActivity implements RecognitionListener{
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +38,11 @@ public class RecipeActivity extends AppCompatActivity implements RecognitionList
         final Recipe recipe = (Recipe) getIntent().getSerializableExtra("recipe");
 
         ImageView img = (ImageView) findViewById(R.id.recipeDetailImg);
-        TextView title = (TextView) findViewById(R.id.recipeDetailTitle);
-        TextView description = (TextView) findViewById(R.id.recipeDetailDesc);
-        ListView ingredientsListView = (ListView) findViewById(R.id.ingredientsListView);
-        ListView directionsListView = (ListView) findViewById(R.id.directionsListView);
+//        TextView title = (TextView) findViewById(R.id.recipeDetailTitle);
+//        TextView description = (TextView) findViewById(R.id.recipeDetailDesc);
+//        ListView ingredientsListView = (ListView) findViewById(R.id.ingredientsListView);
+//        ListView directionsListView = (ListView) findViewById(R.id.directionsListView);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,23 +61,54 @@ public class RecipeActivity extends AppCompatActivity implements RecognitionList
                 .load(recipe.imgUrl)
                 .fit()
                 .into(img);
+//
+//        title.setText(recipe.title);
+//        description.setText(recipe.description);
+//
+//        ingredientsListView.setAdapter(new ArrayAdapter<>(
+//                RecipeActivity.this,
+//                android.R.layout.simple_list_item_1,
+//                android.R.id.text1,
+//                recipe.ingredients
+//        ));
+//
+//        directionsListView.setAdapter(new ArrayAdapter<>(
+//                RecipeActivity.this,
+//                android.R.layout.simple_list_item_1,
+//                android.R.id.text1,
+//                recipe.directions
+//        ));
 
-        title.setText(recipe.title);
-        description.setText(recipe.description);
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.expandableList);
 
-        ingredientsListView.setAdapter(new ArrayAdapter<>(
-                RecipeActivity.this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                recipe.ingredients
-        ));
+        // preparing list data
+        prepareListData(recipe.description, recipe.ingredients, recipe.directions);
 
-        directionsListView.setAdapter(new ArrayAdapter<>(
-                RecipeActivity.this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                recipe.directions
-        ));
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+    }
+
+    private void prepareListData(String description, String[] ingredients, String[] directions) {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("Description");
+        listDataHeader.add("Ingredients");
+        listDataHeader.add("Directions");
+
+        List<String> descriptionList = new ArrayList<>();
+        descriptionList.add(description);
+
+        List<String> ingredientsList = Arrays.asList(ingredients);
+        List<String> directionsList = Arrays.asList(directions);
+
+        listDataChild.put(listDataHeader.get(0), descriptionList);
+        listDataChild.put(listDataHeader.get(1), ingredientsList);
+        listDataChild.put(listDataHeader.get(2), directionsList);
 
     }
 
